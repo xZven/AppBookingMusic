5a649176cae904853caf3eb914c88ba218a0d863
Cyril Cristofol <cyril.cristofol@gmail.com>
2016-11-29 11:46:26 +0100

    Creation Layout + bouttons

--- app/src/main/java/net/stri/m2/appbookingmusic/StartActivity.java
+++ app/src/main/java/net/stri/m2/appbookingmusic/StartActivity.java
@@ -13,8 +13,6 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         //********************************************//
      }
