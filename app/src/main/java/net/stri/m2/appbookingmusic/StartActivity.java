ea84ddcb6f3d589d65f93454f06521a7d63ad12c
xZven <manavai.teikituhaahaa@gmail.com>
2016-12-05 17:23:11 +0100

    Changement de vue lors des appuuies sur les boutons s'identifier et s'inscrire.

--- app/src/main/java/net/stri/m2/appbookingmusic/StartActivity.java
+++ app/src/main/java/net/stri/m2/appbookingmusic/StartActivity.java
@@ -1,15 +1,20 @@
 package net.stri.m2.appbookingmusic;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
 public class StartActivity extends AppCompatActivity {
 
-    private ProgressBar pBar         = null ;
+    private ProgressBar pBar         = null;
     private Button buttonSidentifier = null;
+    private Button buttonSinscrire   = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +23,9 @@ public class StartActivity extends AppCompatActivity {
         //********************************************//
 
         this.pBar = (ProgressBar) findViewById(R.id.AppBarProgressBar);
-        // button
+        // Recensement des buttons
         this.buttonSidentifier = (Button) findViewById(R.id.buttonConnexion);
+        this.buttonSinscrire   = (Button) findViewById(R.id.buttonConnexion);
      }
 
 
@@ -40,13 +46,31 @@ public class StartActivity extends AppCompatActivity {
     public void sIndentifier(View v) {
 
         this.buttonSidentifier.setEnabled(false);
+        this.buttonSinscrire.setEnabled(true);
+        //
+        RelativeLayout mainContent = (RelativeLayout) findViewById(R.id.fragment);
+        LayoutInflater inflater    = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rootView              = inflater.inflate(R.layout.fragment_start, mainContent, false);
+        // création de la vue qui sera ajouté
+        View           layout      = inflater.inflate(R.layout.fragment_connexion, null);
+        //
+        mainContent.removeAllViews();
+        mainContent.addView(layout);
+    }
 
-        // on change le fragment d'affichage (marche pas)
-/*
-        ViewStub stub = (ViewStub) findViewById(R.id.contentIncluder);
-        stub.setLayoutResource(R.layout.fragment_connexion);
-        View inflated = stub.inflate();
-*/
+    public void sInscrire(View v){
+
+        this.buttonSidentifier.setEnabled(true);
+        this.buttonSinscrire.setEnabled(false);
+        //
+        RelativeLayout mainContent = (RelativeLayout) findViewById(R.id.fragment);
+        LayoutInflater inflater    = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rootView              = inflater.inflate(R.layout.fragment_start, mainContent, false);
+        // création de la vue qui sera ajouté
+        View           layout      = inflater.inflate(R.layout.fragment_inscription, null);
+        //
+        mainContent.removeAllViews();
+        mainContent.addView(layout);
     }
 
 }
